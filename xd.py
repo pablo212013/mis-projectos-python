1268c35aa8437a9900a48253d4e54d6bd73dde27
pablo212013 <josepablo1212013@gmail.com>
2024-09-12 20:51:15 -0500

    Add files via upload

---
+++ xd.py
@@ -0,0 +1,24 @@
+print (" sistema para calcular promedio de un alumno ")
+
+nombre = input("cual es tu nombre? : ")
+
+print (nombre + (" inserta tus calificaciones de las siguientes materias") )
+
+matematicas = bool(input("matematicas: ") )
+ingles = bool(input("ingles: ") )
+naturales = bool(input("naturales: ") )
+
+promedio = (matematicas + ingles + naturales) / 3
+
+
+if promedio >= 3:
+    print (nombre + ", pasaste el año con un promedio de: ",  promedio )
+
+print ("fin.")
+
+if promedio <= 2:
+    print (nombre + ", perdiste el año con un promedio de: ",  promedio )
+    print ("fin")
+
+
+
